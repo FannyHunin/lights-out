2c1f9f098b922bf0d187504229334ec927bf32d6
fanny <fanny.hunin.an123@gmail.com>
2021-12-13 16:27:01 +0100

    Updates

---
+++ precache-manifest.f80cd8fa4b1750400b5c6a154cd79156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56740cdbe17c02473442",
+    "url": "/lights-out/static/css/main.5576002c.chunk.css"
+  },
+  {
+    "revision": "56740cdbe17c02473442",
+    "url": "/lights-out/static/js/main.56740cdb.chunk.js"
+  },
+  {
+    "revision": "5221334f8ce7fd087753",
+    "url": "/lights-out/static/js/1.5221334f.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "efb311cd6f33fc2e53160f950db20749",
+    "url": "/lights-out/static/media/select.efb311cd.mp3"
+  },
+  {
+    "revision": "108fd2e5ee5e0acb37e8c47c9339f091",
+    "url": "/lights-out/static/media/win.108fd2e5.mp3"
+  },
+  {
+    "revision": "0d6acbd84a471a60a74db2cdf69c31d0",
+    "url": "/lights-out/index.html"
+  }
+];
